d9e2ed675443cf93312098214baf87ef85956c3e
Arsenije Savic <djxbserbia@gmail.com>
2018-06-07 13:38:52 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,14 @@ module.exports = {
         name: 'pages',
       },
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/assets/images`,
-        name: 'images',
-      },
-    },
+
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     path: `${__dirname}/src/assets/images`,
+    //     name: 'images',
+    //   },
+    // },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
