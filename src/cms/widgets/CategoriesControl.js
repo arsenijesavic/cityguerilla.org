a1d5d33926088da5adbe7c9c0300b12829465197
Arsenije Savic <djxbserbia@gmail.com>
2018-06-08 11:58:54 +0200

    ...

--- src/cms/widgets/CategoriesControl.js
+++ src/cms/widgets/CategoriesControl.js
@@ -7,15 +7,35 @@ class CategoriesControl extends Component {
 
   render() {
     console.log(this.props.entry)
-    var entry = this.props.entry;
-    var posts = entry.getIn(['data', 'actions']);
-    var bg = this.props.getAsset(posts);
-    console.log(posts,bg )
-
+    console.log(this.props)
     return (
       <input type="text"  class="nc-controlPane-widget" value={this.props.value} onChange={this.handleChange}  />
+
     )
   }
 }
 
 export default CategoriesControl
+
+
+// import React, {Component} from 'react'
+
+// class CategoriesControl extends Component {
+
+//   handleChange = e =>
+//     this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
+
+//   render() {
+//     console.log(this.props.entry)
+//     var entry = this.props.entry;
+//     var posts = entry.getIn(['data', 'actions']);
+//     var bg = this.props.getAsset(posts);
+//     console.log(posts,bg )
+
+//     return (
+//       <input type="text"  class="nc-controlPane-widget" value={this.props.value} onChange={this.handleChange}  />
+//     )
+//   }
+// }
+
+// export default CategoriesControl
