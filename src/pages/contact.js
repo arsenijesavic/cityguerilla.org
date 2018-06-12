1dac19b0f4a129d8071707ae8042903f003d1836
Arsenije Savic <djxbserbia@gmail.com>
2018-06-12 09:38:38 +0200

    ...

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,43 +1,65 @@
 import React from 'react'
 import { Grid, Cell } from '../components'
+import cn from 'classnames'
 
-const AboutPage = () => (
+const ContactPage = () => (
   <Grid>
     <Cell width={16} height={16} top={2} left={2}>
-      <div className="wrap">
-        <div className="mat-div">
-          <label htmlFor="first-name: " className="mat-label">
-            First & Last Name
-          </label>
-          <input type="text" className="mat-input" id="first-name: " />
-        </div>
-
-        <div className="mat-div">
-          <label htmlFor="first-name: " className="mat-label">
-            Email
-          </label>
-          <input type="text" className="mat-input" id="last-name: " />
-        </div>
-
-        <div className="mat-div">
-          <label htmlFor="address" className="mat-label">
-            Title
-          </label>
-          <input type="text" className="mat-input" id="address" />
-        </div>
-        <div className="mat-div">
-          <label htmlFor="address" className="mat-label">
-            your message...
-          </label>
-          <textarea name="message" className="mat-input" rows="10" cols="40" />
-        </div>
-        <button>Submit</button>
-      </div>
+      <form style={{ width: '450px', margin: '0 auto' }} name="contact" method="POST" netlify>
+        <Input type='text' label='First & Last Name' />
+        <Input type='text' label='Email' />
+        <Input type='text' label='Title' />
+        <Input type='textarea' label=' your message...' />
+        <div data-netlify-recaptcha />
+        <button type='submit'>Submit</button>
+      </form>
     </Cell>
-  </Grid>
+  </Grid >
 )
 
-export default AboutPage
+export default ContactPage
+
+class Input extends React.Component {
+
+  state = {
+    isFocused: false
+  }
+
+  render() {
+    const { isFocused } = this.state
+    const { id, label, type } = this.props
+
+    const types = {
+      text: <input
+        ref={node => this.input = node}
+        type="text"
+        id="first-name: "
+        className="mat-input"
+        onFocus={() => this.setState({ isFocused: true })}
+        onBlur={() => this.setState({ isFocused: false })}
+      />,
+      textarea: <textarea
+        ref={node => this.input = node}
+        name="message"
+        className="mat-input"
+        rows="10"
+        cols="40"
+        onFocus={() => this.setState({ isFocused: true })}
+        onBlur={() => this.setState({ isFocused: false })}
+      />
+    }
+    return (
+      <div className={cn('mat-div', { 'is-active': isFocused, 'is-completed': isFocused || this.input && this.input.value !== '' })}>
+        <label htmlFor="first-name: " className="mat-label">
+          {label}
+        </label>
+        {types[type]}
+      </div>
+    )
+  }
+}
+
+
 
 // function contactFormAnimation() {
 //   $(".mat-input").focus(function () {
