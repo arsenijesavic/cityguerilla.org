f71005749ce480c7f3e3a9c8f42d717e7ea1789c
Arsenije Savic <djxbserbia@gmail.com>
2018-06-17 19:14:38 +0200

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,9 +52,10 @@ const IndexPage = ({ data }) => {
         <div
           style={{
             width: '100%',
+            height: '100%',
+            overflow: 'hidden',
             padding: '15px 15px',
             textTransform: 'lowercase',
-            //fontSize: '0.707em'
           }}
         >
           {tags &&
