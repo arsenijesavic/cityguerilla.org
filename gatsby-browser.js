9475a1f6f2b58d551ba79b842960636c39466baa
Arsenije Savic <djxbserbia@gmail.com>
2018-06-21 16:33:52 +0200

    ...

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
-let i = 0
+// let i = 0
 
-exports.onClientEntry = a => {
-  window.___emitter.on(`onPostLoadPageResources`, () => {
-    i++
-    i = i % 3
-    if (i === 1) window.isLoading = true
-    else window.isLoading = false
-  })
-}
+// exports.onClientEntry = a => {
+//   window.___emitter.on(`onPostLoadPageResources`, () => {
+//     i++
+//     i = i % 3
+//     if (i === 1) window.isLoading = true
+//     else window.isLoading = false
+//   })
+// }
